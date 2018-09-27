c09199b382a5585e21d830dda2073d4f9b30f221
ionawright <ionawright@live.com>
2018-09-27 14:31:34 +0100

    created_tables

---
+++ db/imdb.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS actors ;
+DROP TABLE IF EXISTS movies ;
+DROP TABLE IF EXISTS roles ;
+
+CREATE TABLE actors(
+  id SERIAL8 PRIMARY KEY,
+  f_name VARCHAR(255),
+  l_name VARCHAR(255)
+);
+
+CREATE TABLE movies(
+  id SERIAL8 PRIMARY KEY,
+  title VARCHAR(255),
+  genre VARCHAR(255)
+);
+
+CREATE TABLE roles(
+  id SERIAL8 PRIMARY KEY,
+  actor_id INT8 REFERENCES actors(id) ON DELETE CASCADE,
+  movie_id INT8 REFERENCES movies(id) ON DELETE CASCADE,
+  fee INT2
+);
